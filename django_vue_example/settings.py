4c82188d744f82a58bce383c54da40e2885225e7
samkim <inspireworld@naver.com>
2019-09-20 04:59:03 +0900

    Django vue integration for heroku

--- django_vue_example/settings.py
+++ django_vue_example/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'dist'),
+  os.path.join(BASE_DIR, 'dist/static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
